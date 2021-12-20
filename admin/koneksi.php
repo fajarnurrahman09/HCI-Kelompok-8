85b3568227eb3d81cf285e0071290d2c0a181c24
fajarnurrahman09 <62900256+fajarnurrahman09@users.noreply.github.com>
2021-12-20 11:49:44 +0700

    feat: koneksi database

--- admin/koneksi.php
+++ admin/koneksi.php
@@ -1,6 +1,7 @@
 
 <?php
-$koneksi = mysqli_connect("localhost", "root", "", "desabinaan");
+$koneksi = mysqli_connect("sql113.epizy.com", "epiz_30624753", "Tqm8uxZTVt");
+mysqli_select_db($koneksi, "epiz_30624753_desabinaan")
 
 
 ?>
